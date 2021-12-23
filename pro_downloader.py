3ed2a7f1811714b890943fae2dddf755273fe076
Carol Braga <95436862+carolinefebraga@users.noreply.github.com>
2021-12-23 12:04:56 -0300

    Update pro_downloader.py

--- pro_downloader.py
+++ pro_downloader.py
@@ -2,10 +2,13 @@ import requests
 
 def baixar_arquivo(url,endereco):
 	resposta = requests.get(url)
-	with open (endereco, 'wb') as novo_arquivo:
-		novo_arquivo.write(resposta.content)
+	if resposta.status_code == requests.codes.OK:
+		with open(endereco, 'wb') as novo_arquivo:
+			novo_arquivo.write(resposta.content)
+		print("Download finalizado. Nome do arquivo: {}".format(endereco))
+	else:
+		resposta.raise_for_status()	
 
 
-
-if __name__=="__main__":
-	baixar_arquivo('https://wp.ufpel.edu.br/seginfo/files/2016/06/OUCH-201510_pt.pdf', 'Gerenciamento de senhas.pdf')
+if __name__ == "__main__":
+	baixar_arquivo('https://esic.br/manuais2021/Pol%C3%ADticadeloginEsenha.pdf', 'Gerenciamento de Senhas.pdf')
